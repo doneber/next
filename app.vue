f6ea8c8e22e964d84aa9fa9bc1a573461230e320
Eber <52986565+doneber@users.noreply.github.com>
2023-01-18 00:29:04 -0400

    🎨 style: Add basic global styles

--- app.vue
+++ app.vue
@@ -3,3 +3,34 @@
     <NuxtPage />
   </div>
 </template>
+
+<style>
+body {
+  background-color: rgb(40, 40, 44);
+}
+
+main {
+  width: min(980px, 100% - 2em);
+  margin: auto;
+}
+h1,
+p {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  color: whitesmoke;
+}
+
+h1 {
+  font-size: xxx-large;
+}
+p {
+  font-size: large;
+}
+span.thin-weight {
+  font-weight: 500;
+}
+
+span.title {
+  font-weight: bold;
+  color: rgb(0, 150, 150);
+}
+</style>
