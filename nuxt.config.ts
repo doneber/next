034cfe9529bb6ca3ea76f77d5d4e6d87f5e09de5
Eber <52986565+doneber@users.noreply.github.com>
2023-01-22 17:17:52 -0400

    🔧 config: Set nuxt.config.ts code highlight theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content']
+  modules: ['@nuxt/content'],
+  content: {
+    highlight: {
+      theme: 'dark-plus',
+      // theme: 'github-dark'
+    }
+  }
 })
